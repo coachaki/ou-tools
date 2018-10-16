554bbc2ebdc3478aac831c4d6917cedac0f4f116
Akifumi Yamamoto <af.yamamoto@gmail.com>
2018-10-16 00:50:30 -0700

    finding ways to access the current user session for ou campus

--- chrome/background.js
+++ chrome/background.js
@@ -7,9 +7,14 @@ chrome.runtime.onInstalled.addListener(function() {
 
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
         chrome.declarativeContent.onPageChanged.addRules([{
-            conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: { hostEquals: 'developer.chrome.com' },
-            })],
+            conditions: [
+                new chrome.declarativeContent.PageStateMatcher({
+                    pageUrl: { hostEquals: 'developer.chrome.com' }
+                }),
+                new chrome.declarativeContent.PageStateMatcher({
+                    css: ["a#nav-help[href*=oucampus10]", "a.brand[href*='/10/']"] // check for a link to the support site and a link to v10 ou campus
+                })
+            ],
             actions: [new chrome.declarativeContent.ShowPageAction()]
         }]);
     });
