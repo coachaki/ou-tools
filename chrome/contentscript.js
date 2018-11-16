aaae4b838c2253d1ceb28d3f8dd3398be4ec6054
Akifumi Yamamoto <af.yamamoto@gmail.com>
2018-11-15 21:30:32 -0800

    added test scripts to contentscript.js

--- chrome/contentscript.js
+++ chrome/contentscript.js
@@ -1,3 +1,16 @@
+// None of them really work yet.
+
+var query = function(endpoint, params, method = 'get') {
+    let xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', function (evt) {
+        console.log(JSON.parse(this.response));
+        console.log(evt);
+    });
+
+    xhr.open(method, endpoint);
+    // xhr.send(params); // TODO: need to make sure this gets sent in the proper format
+};
+
 var getSites = function(account) {
     let xhr = new XMLHttpRequest();
     xhr.addEventListener('load', function(evt) {
@@ -10,4 +23,23 @@ var getSites = function(account) {
 
     xhr.open('get', api);
     xhr.send('account=' + account);
+};
+
+var getProperties = function(path) {
+    let endpoint = '/files/properties';
+    let method = 'get';
+
+    query(endpoint, path, method);
+};
+
+var getParams = function(params) {
+    var output = '';
+    params.parameters.forEach(function (field) {
+        if (field.section) {
+            output += '- ' + field.section + '\n';
+        }
+        output += '\t- **' + field.prompt + '**: ' + field.alt + '\n';
+    })
+    console.log(output);
+    return output;
 };
