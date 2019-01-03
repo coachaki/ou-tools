17e00592202f1aa0c661033dd2354f0d091c91fd
Akifumi Yamamoto <ayamamoto@omniupdate.com>
2019-01-02 16:05:24 -0800

    some progress to get first function working

--- chrome/popup.js
+++ chrome/popup.js
@@ -1,9 +1,53 @@
+/* global templates */
+
+const appUI = document.getElementById('app-ui');
+
+function getSites() {
+  const xhr = new XMLHttpRequest();
+  xhr.addEventListener('load', (evt) => {
+    console.log(evt);
+  });
+  chrome.tabs.query({
+    active: true,
+    currentWindow: true,
+  }, (tabs) => {
+    const tab = tabs[0];
+    const {
+      url,
+    } = tab;
+    console.log(url);
+
+    chrome.cookies.getAll({
+      url,
+    }, (cookies) => {
+      console.log(cookies);
+    });
+
+    chrome.tabs.executeScript(tab.id, {
+      code: 'getSites("clpccd");',
+    });
+  });
+}
+
+function outputUI(action) {
+  let output = '';
+  switch (action) {
+    case 'newUser':
+      output = templates.dev.newUser;
+      break;
+    default:
+      console.log('unknown action');
+  }
+
+  appUI.innerHTML = output;
+}
+
 const changeColor = document.getElementById('changeColor');
 chrome.storage.sync.get('color', (data) => {
   changeColor.style.backgroundColor = data.color;
   changeColor.setAttribute('value', data.color);
 });
-changeColor.onclick = function (element) {
+changeColor.onclick = (element) => {
   const color = element.target.value;
   chrome.tabs.query({
     active: true,
@@ -16,28 +60,4 @@ changeColor.onclick = function (element) {
   getSites();
 };
 
-let getSites = function () {
-  const xhr = new XMLHttpRequest();
-  xhr.addEventListener('load', (evt) => {
-    console.log(evt);
-  });
-  chrome.tabs.query({
-    active: true,
-    currentWindow: true,
-  }, (tabs) => {
-    const tab = tabs[0];
-    const url = tab.url;
-    console.log(url);
-
-    chrome.cookies.getAll({
-      url,
-    }, (cookies) => {
-      console.log(cookies);
-    });
-
-    chrome.tabs.executeScript(tab.id, {
-      code: 'getSites("ayamamoto");',
-    });
-  });
-};
 // $.get('/sites/list', { "account" : "TERRY" }).done(function(response) { console.log(response); })
