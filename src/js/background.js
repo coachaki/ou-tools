c95727b01e3a43e03aa8855872a5f24652e75960
Akifumi Yamamoto <ayamamoto@omniupdate.com>
2019-01-14 13:36:03 -0800

    making an api object for easy querying using fetch

--- src/js/background.js
+++ src/js/background.js
@@ -1,21 +1,21 @@
-'use strict';
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ color: '#3aa757' }, () => {
+    console.log('The color is green.');
+  });
 
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({ color: '#3aa757' }, function() {
-        console.log('The color is green.');
-    });
-
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-        chrome.declarativeContent.onPageChanged.addRules([{
-            conditions: [
-                new chrome.declarativeContent.PageStateMatcher({
-                    pageUrl: { hostEquals: 'a.cms.omniupdate.com' }
-                }),
-                new chrome.declarativeContent.PageStateMatcher({
-                    css: ['a#nav-help[href*=oucampus10]', 'a.brand[href*="/10/"]'] // check for a link to the support site and a link to v10 ou campus
-                })
-            ],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-        }]);
-    });
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: 'a.cms.omniupdate.com' },
+          }),
+          new chrome.declarativeContent.PageStateMatcher({
+            css: ['a#nav-help[href*=oucampus10]', 'a.brand[href*="/10/"]'], // check for a link to the support site and a link to v10 ou campus for non-sass instances
+          }),
+        ],
+        actions: [new chrome.declarativeContent.ShowPageAction()],
+      },
+    ]);
+  });
 });
