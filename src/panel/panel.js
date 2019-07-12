76a284e5927713ea170184236c5982a25f175cb8
Akifumi Yamamoto <af.yamamoto@gmail.com>
2019-07-12 08:03:59 -0700

    figuring out how to work w/ web-ext

--- src/panel/panel.js
+++ src/panel/panel.js
@@ -6,17 +6,11 @@ const theme = panels.themeName;
 
 console.log(browser, window);
 
-function test(requestDetails) {
-  console.log(requestDetails);
-}
-
-// browser.webRequest.onBeforeRequest.addListener(test);
-
-fetch('/authentication/whoami')
-  .then(res => res.json())
-  .then(json => console.log(json));
+// fetch('/authentication/whoami')
+//   .then(res => res.json())
+//   .then(json => console.log(json));
 
 inspectedWindow.eval(call1)
   .then((result) => {
-    console.log(result[0]);
+    console.log(result);
   });
