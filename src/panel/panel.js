4583dcf2804c3b4b464b65dc1127d13ac366b799
Akifumi Yamamoto <ayamamoto@omniupdate.com>
2019-07-15 15:05:22 -0700

    added code to create connections between devtools panels and background script

--- src/panel/panel.js
+++ src/panel/panel.js
@@ -1,9 +1,32 @@
-/* global browser */
-
 const { inspectedWindow, panels, network } = browser.devtools;
 
+const bgScript = browser.runtime.connect({ name: `panel-js${inspectedWindow.tabId}` });
+
 console.log(browser, window);
+
+const url = () => {
+  const tab = inspectedWindow.tabId;
+  browser.runtime.sendMessage({ tabId: tab, action: 'get url' });
+  return '';
+};
+
+url();
 
 fetch('https://a.cms.omniupdate.com/authentication/whoami')
   .then(res => res.json())
   .then(json => console.log('whoami', json));
+
+bgScript.onMessage.addListener((message) => {
+  switch (message.type) {
+    case 'get': {
+      const data = window[message.callback]();
+      bgScript.postMessage({ type: 'set', name: 'tabId', data });
+      break;
+    }
+    default:
+  }
+});
+
+function getInspectedTabId() {
+  return inspectedWindow.tabId;
+}
