22c74c8f2327470b88288f25a4df9f32a03fe33d
Akifumi Yamamoto <af.yamamoto@gmail.com>
2019-07-23 06:27:56 -0700

    remove handlers from main.js

--- shells/webextension/main.js
+++ shells/webextension/main.js
@@ -3,13 +3,13 @@ This script is run whenever the devtools are open.
 In here, we can create our panel.
 */
 
-function handleShown() {
-  console.log('panel is being shown');
-}
+// function handleShown() {
+//   console.log('panel is being shown');
+// }
 
-function handleHidden() {
-  console.log('panel is being hidden');
-}
+// function handleHidden() {
+//   console.log('panel is being hidden');
+// }
 
 /**
 Create a panel, and add listeners for panel show/hide events.
@@ -24,10 +24,10 @@ browser.devtools.inspectedWindow.eval('typeof OU != "undefined"').then((result) 
     }
 
     browser.devtools.panels
-      .create('OU Campus', '/img/icons/ou-tools-48.png', '/panel/panel.html')
-      .then((newPanel) => {
-        newPanel.onShown.addListener(handleShown);
-        newPanel.onHidden.addListener(handleHidden);
-      });
+      .create('OU Campus', '/img/icons/ou-tools-48.png', '/panel/panel.html');
+    // .then((newPanel) => {
+    //   newPanel.onShown.addListener(handleShown);
+    //   newPanel.onHidden.addListener(handleHidden);
+    // });
   }
 });
