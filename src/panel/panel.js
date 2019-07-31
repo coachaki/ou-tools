c3f3085c16430a85718c2daaaa1c11913438b5da
Akifumi Yamamoto <ayamamoto@omniupdate.com>
2019-07-31 15:58:56 -0700

    making progress towards the first feature

--- src/panel/panel.js
+++ src/panel/panel.js
@@ -1,18 +1,64 @@
 const { inspectedWindow, panels, network } = browser.devtools;
 const browserData = {};
 const ouc = {};
-
 const bgScript = browser.runtime.connect({ name: `paneljs${inspectedWindow.tabId}` });
-bgScript.postMessage({ api: 'tabs', method: 'get', argv: [1] });
-bgScript.onMessage.addListener((m) => {
-  if (m.method === 'tabs.get') {
-    browserData.tab = m.response;
-    const url = new URL(browserData.tab.url);
-    ouc.info.apihost = url.origin;
 
-    fetch(`${ouc.info.apihost}/authentication/whoami`)
+const panel = {
+  async getTabInfo() {
+    bgScript.postMessage({ api: 'tabs', method: 'get', argv: [1] });
+  },
+  getMeList() {
+    const dl = document.createElement('dl');
+    dl.classList = ['horizontal'];
+    const outputList = ['skin', 'account', 'site', 'username'];
+    outputList.forEach((value) => {
+      const dt = document.createElement('dt');
+      dt.innerText = value;
+      const dd = document.createElement('dd');
+      dd.innerText = ouc.me[value];
+      dl.append(dt, dd);
+    });
+    return dl;
+  },
+  getSite(url) {
+    return url.substr(url.indexOf('#')).split('/')[2];
+  },
+  async whoami() {
+    return fetch(`${ouc.me.apihost}/authentication/whoami`)
       .then(res => res.json())
-      .then(json => console.log('whoami', json));
-  }
-  console.log(data);
-});
+      .then((json) => {
+        const site = panel.getSite(browserData.tab.url);
+        Object.assign(ouc.me, json, { username: json.user.username, site });
+      });
+  },
+};
+
+function addListeners() {
+  bgScript.onMessage.addListener((m) => {
+    if (m.method === 'tabs.get') {
+      browserData.tab = m.response;
+      const url = new URL(browserData.tab.url);
+      ouc.me = {
+        apihost: url.origin,
+      };
+      panel.whoami().then(() => {
+        const meList = panel.getMeList();
+        const info = document.getElementById('tabInfo');
+        info.removeChild(...info.children);
+        info.append(meList);
+      });
+    }
+  });
+  window.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById('whoami').addEventListener('click', (ev) => {
+      panel.getTabInfo();
+    });
+    document.addEventListener('submit', (ev) => {
+      ev.preventDefault();
+      console.log(ev);
+    });
+  });
+}
+
+addListeners();
+panel.getTabInfo();
